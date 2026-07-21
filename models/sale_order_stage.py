84a01e27fb1fe30dc25dd93d0429f3978651e3c3
waqasmustafa <mustafawaqas0@gmail.com>
2026-07-21 19:29:14 +0500

    Add new feature

--- models/sale_order_stage.py
+++ models/sale_order_stage.py
@@ -7,3 +7,14 @@ class SaleOrderStage(models.Model):
     is_store_stage = fields.Boolean(string="Store Team Stage", default=False)
     is_purchase_stage = fields.Boolean(string="Purchase Team Stage", default=False)
     is_logistics_stage = fields.Boolean(string="Logistics Team Stage", default=False)
+
+    is_current_user_stage_admin = fields.Boolean(
+        compute='_compute_is_current_user_stage_admin'
+    )
+
+    @api.depends_context('uid')
+    def _compute_is_current_user_stage_admin(self):
+        is_admin = self.env.su or self.env.user.has_group('base.group_system') or self.env.user.is_admin_stage_team
+        for stage in self:
+            stage.is_current_user_stage_admin = is_admin
+
